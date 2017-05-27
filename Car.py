02692db18d2960a1e48b1e9b5140b9a259ddf5b3
zhangletian <1007174589@qq.com>
2017-05-28 07:44:29 +0800

    Car.py

---
+++ Car.py
@@ -0,0 +1,41 @@
+class Car():
+	def __init__(self,make,model,year):
+		self.make = make
+		self.model = model
+		self.year = year
+		self.odometer_reading = 0
+		
+	def get_descriptive_name(self):
+		long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+		return long_name.title()
+
+	def read_odometer(self):
+		print('This car has ' + str(self.odometer_reading) + ' miles on it.')
+
+	def update_odometer(self,mileage):
+		if mileage > self.odometer_reading:
+			self.odometer_reading = mileage
+
+	def increment_odometer(self,miles):
+		if miles > 0:
+			self.odometer_reading += miles
+			
+class ElectricCar(Car):
+	def __init__(self,make,model,year):
+		super().__init__(make,model,year)
+		self.battery_size = 70
+	
+	def describe_battery(self):
+		print('This car has a ' + str(self.battery_size) + '-kWh battery')
+
+my_tesla = ElectricCar('tesla','model s',2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+print(my_tesla.describe_battery())
+
+my_new_car = Car('audi','a4',2016)
+my_new_car.odometer_reading = 23
+my_new_car.update_odometer(24)
+
+print(my_new_car.get_descriptive_name())
+print(my_new_car.read_odometer())
