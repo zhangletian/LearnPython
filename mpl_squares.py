ab8b2c3af3ab6dd8f77979c1ede057202f671e3c
zhangletian <1007174589@qq.com>
2017-06-07 22:38:28 +0800

    数据可视化学习

--- mpl_squares.py
+++ mpl_squares.py
@@ -9,7 +9,10 @@ import matplotlib.pyplot as plt
 
 x_values=list(range(1,1001))
 y_values=[i**2 for i in x_values]
-plt.scatter(x_values,y_values,c='red',edgecolor='none',s=10)
+#plt.scatter(x_values,y_values,c='red',edgecolor='none',s=10)
+#plt.scatter(x_values,y_values,c=(0,0,0.5),edgecolor='none',s=40)
+plt.scatter(x_values,y_values,c=y_values,cmap=plt.cm.Blues,
+	edgecolor='none',s=40)
 
 #设置图表标题，并给坐标轴加上标签
 plt.title('Square Numbers',fontsize=24)
@@ -19,4 +22,5 @@ plt.ylabel('Square of value',fontsize=14)
 #设置刻度标记的大小
 plt.tick_params(axis='both',which='major',labelsize=14)
 
-plt.show()
+#plt.show()
+plt.savefig('squares_plot.png',bbox_inches='tight')
