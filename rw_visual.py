1edd70edbdfe699a25e974e6547e8aa28555ee21
zhangletian <1007174589@qq.com>
2017-06-07 23:51:56 +0800

    重新绘制起点和终点

--- rw_visual.py
+++ rw_visual.py
@@ -8,7 +8,16 @@ while True:
 	#创见一个RandomWalk实例，并将其包含的点都绘制出来
 	rw = RandomWalk()
 	rw.fill_walk()
-	plt.scatter(rw.x_values,rw.y_values,s=15)
+
+	point_numbers = list(range(rw.num_points))
+	plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,
+		edgecolor='none',s=15)
+	
+	#突出起点与终点
+	plt.scatter(0,0,c='green',edgecolor='none',s=100)
+	plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolor='none',
+		s=100)
+		
 	plt.show()
 
 	keep_running = input('make another walk?(y/n):')
