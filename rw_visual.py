eb59629ff74b00ef453dffdff884b05245ec43ef
zhangletian <1007174589@qq.com>
2017-06-08 22:36:58 +0800

    调整尺寸以适合屏幕

--- rw_visual.py
+++ rw_visual.py
@@ -6,12 +6,15 @@ from random_walk import RandomWalk
 #只要程序处于活动状态，就不断地模拟随机漫步
 while True:
 	#创见一个RandomWalk实例，并将其包含的点都绘制出来
-	rw = RandomWalk()
+	rw = RandomWalk(50000)
 	rw.fill_walk()
 
+	#设置绘图窗口的尺寸
+	plt.figure(dpi=256,figsize=(20,10))
+	
 	point_numbers = list(range(rw.num_points))
 	plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,
-		edgecolor='none',s=15)
+		edgecolor='none',s=1)
 	
 	#突出起点与终点
 	plt.scatter(0,0,c='green',edgecolor='none',s=100)
