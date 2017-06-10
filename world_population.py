28c1f8c87bcd00da1f3e6f6daf36d48c89020983
zhangletian <1007174589@qq.com>
2017-06-11 00:26:21 +0800

    制作世界人口地图

---
+++ world_population.py
@@ -0,0 +1,16 @@
+import json
+from country_codes import get_country_code
+
+filename = 'population_data.json'
+with open(filename) as f:
+	pop_data = json.load(f)
+
+for pop_dict in pop_data:
+	if pop_dict['Year'] == '2010':
+		country_name = pop_dict['Country Name']
+		population = int(float(pop_dict['Value']))
+		code = get_country_code(country_name)
+		if code:
+			print(code + ':' + str(population))
+		else:
+			print('ERROR - ' + country_name)
