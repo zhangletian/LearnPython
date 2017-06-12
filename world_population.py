fe9f8b349efbee68c5667edff6e539386efc7cc1
zhangletian <1007174589@qq.com>
2017-06-12 22:28:39 +0800

    绘制完整的世界人口地图

--- world_population.py
+++ world_population.py
@@ -1,16 +1,22 @@
 import json
+import pygal
 from country_codes import get_country_code
 
 filename = 'population_data.json'
 with open(filename) as f:
 	pop_data = json.load(f)
 
+cc_populations = {}
 for pop_dict in pop_data:
 	if pop_dict['Year'] == '2010':
 		country_name = pop_dict['Country Name']
 		population = int(float(pop_dict['Value']))
 		code = get_country_code(country_name)
 		if code:
-			print(code + ':' + str(population))
-		else:
-			print('ERROR - ' + country_name)
+			cc_populations[code] = population
+
+wm = pygal.Worldmap()
+wm.title = 'World Population in 2010,by Country'
+wm.add('2010',cc_populations)
+
+wm.render_to_file('world_population.svg')
