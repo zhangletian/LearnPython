d231b6b2a99babe8624edd42e29b0e9396736f7d
zhangletian <1007174589@qq.com>
2017-06-16 12:35:46 +0800

    数据可视化项目

---
+++ Data Visualization/rw_visual.py
@@ -0,0 +1,32 @@
+#coding=gbk
+
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+#只要程序处于活动状态，就不断地模拟随机漫步
+while True:
+	#创见一个RandomWalk实例，并将其包含的点都绘制出来
+	rw = RandomWalk(50000)
+	rw.fill_walk()
+
+	#设置绘图窗口的尺寸
+	plt.figure(dpi=256,figsize=(20,10))
+	
+	point_numbers = list(range(rw.num_points))
+	plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,
+		edgecolor='none',s=1)
+	
+	#突出起点与终点
+	plt.scatter(0,0,c='green',edgecolor='none',s=100)
+	plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolor='none',
+		s=100)
+
+	#隐藏坐标轴
+	plt.axes().get_xaxis().set_visible(False)
+	plt.axes().get_yaxis().set_visible(False)
+	
+	plt.show()
+
+	keep_running = input('make another walk?(y/n):')
+	if keep_running == 'n':
+		break
