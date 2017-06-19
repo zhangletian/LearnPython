a1dafd45d12b79ccd3529c20961d637a73db545c
zhangletian <1007174589@qq.com>
2017-06-19 23:38:26 +0800

    列表

--- 用Python玩转数据/totitle.py
+++ 用Python玩转数据/totitle.py
@@ -1,7 +1,8 @@
 aStr = 'What do you think of this saying "No pain,No gain"?'
-lindex = aStr.index('\"',0,len(aStr))
-rindex = aStr.rindex('\"',0,len(aStr))
-tempStr = aStr[lindex+1:rindex]
+#lindex = aStr.index('\"',0,len(aStr))
+#rindex = aStr.rindex('\"',0,len(aStr))
+#tempStr = aStr[lindex+1:rindex]
+tempStr = aStr.split("\"")[1]
 if tempStr.istitle():
 	print('It is title format.')
 else:
---
+++ 用Python玩转数据/week.py
@@ -0,0 +1,5 @@
+week = ['Monday','Tuseday','Wednesday','Thursday','Friday']
+weekend = ['Satuday','Sunday']
+week.extend(weekend)
+for i,j in enumerate(week):
+	print(i+1,j)
