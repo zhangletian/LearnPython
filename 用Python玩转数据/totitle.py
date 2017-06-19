de49e748fff83d0df569162ac1703f18fd09b846
zhangletian <1007174589@qq.com>
2017-06-19 23:22:19 +0800

    字符串

---
+++ 用Python玩转数据/totitle.py
@@ -0,0 +1,9 @@
+aStr = 'What do you think of this saying "No pain,No gain"?'
+lindex = aStr.index('\"',0,len(aStr))
+rindex = aStr.rindex('\"',0,len(aStr))
+tempStr = aStr[lindex+1:rindex]
+if tempStr.istitle():
+	print('It is title format.')
+else:
+	print('It is not title format.')
+print(tempStr.title())
