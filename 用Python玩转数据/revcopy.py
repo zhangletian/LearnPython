a97d94042a79898ab8c67a9560f0a2fadede992b
zhangletian <1007174589@qq.com>
2017-06-20 23:42:35 +0800

    本地数据获取

---
+++ 用Python玩转数据/revcopy.py
@@ -0,0 +1,17 @@
+filename = 'companies.txt'
+with open(filename,'w') as file_object:
+	#file_object.write('GOOGLE Inc.')
+	#file_object.write('\nMicrosoft Corporation')
+	#file_object.write('\nApple Inc.')
+	#file_object.write('\nFacebook Inc.')
+	file_object.write('GOOGLE Inc.\nMicrosoft Corporation\nApple Inc.\nFacebook Inc.')
+
+f1 = open(r'companies.txt')
+cNames = f1.readlines()
+for i in range(0,len(cNames)):
+	cNames[i] = str(i+1) + ' ' + cNames[i]
+f1.close()
+
+f2 = open(r'scompanies.txt','w')
+f2.writelines(cNames)
+f2.close
