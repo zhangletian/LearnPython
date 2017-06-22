ff2a03038d5394a730b2b250b503cf3fc4b99a91
zhangletian <1007174589@qq.com>
2017-06-22 23:14:28 +0800

    ndarray

---
+++ 用Python玩转数据/ndarray.py
@@ -0,0 +1,20 @@
+import numpy as np
+a = np.array([1,2,3])
+print(a)
+
+b = np.array([(1,2,3),(4,5,6)])
+print(b)
+
+c = np.arange(1,5,0.5)
+print(c)
+
+d = np.random.random((2,2))
+print(d)
+
+e = np.linspace(1,2,10,endpoint=False)
+print(e)
+
+print('\n')
+
+f = np.linspace(1,2,10,endpoint=True)
+print(f)
