96b9df5216a6b74b54e53c1d3824922c01ba3c79
zhangletian <1007174589@qq.com>
2017-06-23 12:54:49 +0800

    变长字典Series

---
+++ 用Python玩转数据/math_numpt.py
@@ -0,0 +1,17 @@
+import time
+import math
+import numpy as np
+
+x = np.arange(0,100,0.01)
+t_m1 = time.clock()
+
+for i,t in enumerate(x):
+	x[i] = math.pow((math.sin(t)),2)
+t_m2 = time.clock()
+y = np.arange(0,100,0.01)
+t_n1 = time.clock()
+y = np.power(np.sin(y),2)
+t_n2 = time.clock()
+
+print('Running time of math:',t_m2-t_m1)
+print('Running time of numpy:',t_n2-t_n1)
