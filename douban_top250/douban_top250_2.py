2a17ec557f9e88840721b6bb461a842acc60c561
zhangletian <1007174589@qq.com>
2017-07-09 22:50:35 +0800

    爬虫

--- douban_top250/douban_top250_2.py
+++ douban_top250/douban_top250_2.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 # url = 'https://movie.douban.com/top250'
 urlList = []
 onlyname = []
+ratingList = []
+peopleList = []
 for i in range(0,250,25):
     url = 'https://movie.douban.com/top250?start={start}&filter='.format(start=i)
     urlList.append(url)
@@ -12,17 +14,29 @@ for i in range(0,250,25):
     bsObj = BeautifulSoup(html)
 
     spanSet = bsObj.findAll('span',attrs = {'class':'title'})
-# print(spanSet)
     for span in spanSet:
         imageName = span.string
         if imageName.find(' / ') == -1:
             onlyname.append(imageName)
+
+    ratingSet = bsObj.findAll('span',attrs={'class':'rating_num'})
+    for rating_num in ratingSet:
+        ratingList.append(rating_num.string)
+
+    peopleSet = bsObj.findAll('div',attrs={'class':'star'})#用兄弟标签
+
 		# print(imageName)
 # print(onlyname)
 
-filename = 'DoubanMoviesTop250.txt'
-top_num = 1
-with open (filename,'w') as file_object:
-    for name in onlyname:
-        file_object.write('Top'+ str(top_num) + ' ' + name+'\n')
-        top_num += 1
+# print(spanSet)
+# print(ratingSet)
+# print(ratingList)
+print(peopleSet)
+
+
+# filename = 'DoubanMoviesTop250.txt'
+# top_num = 1
+# with open (filename,'w') as file_object:
+#     for name in onlyname:
+#         file_object.write('Top'+ str(top_num) + ' ' + name+'\n')
+#         top_num += 1
