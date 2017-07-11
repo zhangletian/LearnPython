f8f73c5f268c2f87be1da596154b4d1e2649857e
zhangletian <1007174589@qq.com>
2017-07-11 21:09:19 +0800

    爬虫

--- douban_top250/DoubanBooksTop250.py
+++ douban_top250/DoubanBooksTop250.py
@@ -5,6 +5,8 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 titleList = []
+ratingList = []
+peonumList = []
 for page in range(0,250,25):
     url = 'https://book.douban.com/top250?start={start}'.format(start=page)
     html = urlopen(url)
@@ -15,23 +17,23 @@ for page in range(0,250,25):
             # print(title.attrs['title'])
             titleList.append(title.attrs['title'])
 
-print(titleList)
-# urlList = []
-# onlyname = []
-# ratingList = []
-# peopleList = []
-# for i in range(0,250,25):
-#     url = 'https://movie.douban.com/top250?start={start}&filter='.format(start=i)
-#     urlList.append(url)
-#     html = urlopen(url)
-#     bsObj = BeautifulSoup(html)
-#
-#     spanSet = bsObj.findAll('span',attrs = {'class':'title'})
-#     for span in spanSet:
-#         imageName = span.string
-#         if imageName.find(' / ') == -1:
-#             onlyname.append(imageName)
-#
-#     ratingSet = bsObj.findAll('span',attrs={'class':'rating_num'})
-#     for rating_num in ratingSet:
-#         ratingList.append(rating_num.string)
+    ratingSet = bsObj.findAll('span',{'class':'rating_nums'})
+    for rating in ratingSet:
+        ratingList.append(rating.string)
+
+    peopleSet = bsObj.findAll('span',{'class':'pl'})
+    for peonum in peopleSet:
+        peonumList.append(peonum)
+    # peopleSet = bsObj.find('span', attrs={"class": "pl"}).string.strip('\r\n ()人评价')
+
+
+# print(titleList)
+# print(ratingList)
+# print(peonumList)
+
+filename = 'DoubanBooksTop250.csv'
+top_num = 1
+with open (filename,'w') as file_object:
+    for (name,rating_num) in zip(titleList,ratingList):
+        file_object.write('Top'+ str(top_num) + ',' + name + ',' + str(rating_num) + '\n')
+        top_num += 1
