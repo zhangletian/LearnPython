371272156409e7cfc2ec5999713c7722822cf863
zhangletian <1007174589@qq.com>
2017-07-12 22:11:04 +0800

    数据可视化

--- Data Visualization/rw_visual.py
+++ Data Visualization/rw_visual.py
@@ -6,7 +6,7 @@ from random_walk import RandomWalk
 #只要程序处于活动状态，就不断地模拟随机漫步
 while True:
 	#创见一个RandomWalk实例，并将其包含的点都绘制出来
-	rw = RandomWalk(50000)
+	rw = RandomWalk(500000)
 	rw.fill_walk()
 
 	#设置绘图窗口的尺寸
