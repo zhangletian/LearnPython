87c02f0e7c5a7c426437cf6ddf1c865d35749446
zhangletian <1007174589@qq.com>
2017-07-19 22:46:12 +0800

    数据可视化

---
+++ Data Visualization/rw_visual2.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from random_walk2 import RandomWalk
+
+while True:
+    rw = RandomWalk(500000)
+    rw.fill_walk()
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolor='none',s=15)
+
+    plt.scatter(0,0,c='green',edgecolor='none',s=100)
+    plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolor='none',s=100)
+
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.savefig('RandomWalk.png')
+    plt.show()
+
+    keep_running = input('Make another walk?(y/n):')
+    if keep_running == 'n':
+        break
